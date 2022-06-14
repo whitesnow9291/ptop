57ceb05f08bf71be3861c07dffcda1bfe579e82b
g <g@gs-iMac.local>
2022-06-14 22:46:23 +0900

    fixed admob module

--- App.tsx
+++ App.tsx
@@ -13,6 +13,32 @@ import { PersistGate } from 'redux-persist/integration/react';
 import { store, persistor } from '@app/redux/store';
 import { request, PERMISSIONS } from 'react-native-permissions'
 
+import mobileAds, { MaxAdContentRating } from 'react-native-google-mobile-ads';
+
+mobileAds()
+  .setRequestConfiguration({
+    // Update all future requests suitable for parental guidance
+    maxAdContentRating: MaxAdContentRating.PG,
+
+    // Indicates that you want your content treated as child-directed for purposes of COPPA.
+    tagForChildDirectedTreatment: true,
+
+    // Indicates that you want the ad request to be handled in a
+    // manner suitable for users under the age of consent.
+    tagForUnderAgeOfConsent: true,
+
+    // An array of test device IDs to allow.
+    testDeviceIdentifiers: ['EMULATOR'],
+  })
+  .then(() => {
+    // Request config successfully set!
+
+    mobileAds()
+      .initialize()
+      .then(adapterStatuses => {
+        // Initialization complete!
+      });
+  });
 const Stack = createNativeStackNavigator();
 
 function MainNavigator() {
--- src/screens/components/Admob.tsx
+++ src/screens/components/Admob.tsx
@@ -1,42 +1,53 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { Platform, View } from 'react-native';
-import {
-    AdMobBanner,
-    AdMobInterstitial,
-    PublisherBanner,
-    AdMobRewarded,
-    setTestDeviceIDAsync,
-} from 'expo-ads-admob';
+import { AppOpenAd, InterstitialAd, RewardedAd, BannerAd, TestIds, AdEventType } from 'react-native-google-mobile-ads';
+
 import { ADMOB_APP_UNIT_ID } from '../../assets/constants';
 
-const AD_INTERVAL = 60 * 2 * 1000
+const AD_INTERVAL = 2 * 1000 * 60
+let unitId = ADMOB_APP_UNIT_ID.IOS
+
+if (Platform.OS === 'android') {
+    unitId = ADMOB_APP_UNIT_ID.ANDROID
+}
+const interestialAd = InterstitialAd.createForAdRequest(unitId);
+
 export const CustomAdMobBanner = (props) => {
 
 
+    const [loaded, setLoaded] = useState(false);
+
     useEffect(() => {
-        showAds()
+        const unsubscribe = interestialAd.addAdEventListener(AdEventType.LOADED, () => {
+            setLoaded(true);
+        });
 
-        AdMobInterstitial.addEventListener('interstitialDidClose', () => {
-            setTimeout(() => {
-                showAds()
-            }, AD_INTERVAL);
-        })
+        // Start loading the interstitial straight away
+        interestialAd.load();
+        // Unsubscribe from events on unmount
         return () => {
-            AdMobInterstitial.removeAllListeners()
+            setLoaded(false)
+            unsubscribe()
+        };
+    }, []);
+    useEffect(() => {
+        const unsubscribe = interestialAd.addAdEventListener(AdEventType.CLOSED, () => {
+            setLoaded(false)
+            setTimeout(() => {
+                interestialAd.load();
+            }, AD_INTERVAL);
+        });
+
+        return () => {
+            unsubscribe()
+        };
+    }, []);
+    useEffect(() => {
+        if (loaded) {
+            interestialAd.show()
         }
-    }, [])
+    }, [loaded])
 
-
-    const showAds = async () => {
-        let unitId = ADMOB_APP_UNIT_ID.IOS
-
-        if (Platform.OS === 'android') {
-            unitId = ADMOB_APP_UNIT_ID.ANDROID
-        }
-        await AdMobInterstitial.setAdUnitID(unitId); // Test ID, Replace with your-admob-unit-id
-        await AdMobInterstitial.requestAdAsync({ servePersonalizedAds: true });
-        await AdMobInterstitial.showAdAsync();
-    }
     return <View>
 
     </View>
